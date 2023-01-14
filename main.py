48cb3bdd494d058b890c50acc5606f793106dc34
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-14 08:27:04 +0300

    Added system setting and app frame

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter
+import customtkinter
+from pytube import YouTube
+
+
+# System settings
+customtkinter.set_appearance_mode("System") # Setting the dark or light mode from the system
+customtkinter.set_default_color_theme("blue")
+
+
+# Our app frame
+app = customtkinter.CTk() # Initializing the app
+app.geometry("720x480") # Setting the app size
